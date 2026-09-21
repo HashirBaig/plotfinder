b5b259b14673270be4b3594c716e07e88c9e7281
HashirBaig <hashirbaig456@gmail.com>
2026-09-21 21:14:31 +0200

    minor change

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,10 +1,11 @@
 import Wrapper from "@/components/Wrapper";
 import BagMap from "@/components/BagMap";
+// import ContactCard from "@/components/ContactCard";
 
 function LandingPage() {
   return (
     <Wrapper classnames="p-0 m-0">
-      <BagMap />;
+      <BagMap />;{/* <ContactCard /> */}
     </Wrapper>
   );
 }
